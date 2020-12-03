9ee587f5b7a4d3a1daf1b788b26fb7ecf4d5683f
malleswarchinta92 <malleswar.chinta@gmail.com>
2020-12-03 20:34:12 +0530

    loader and error modal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,30 +3,41 @@ import React, {useState, useCallback} from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([])
+  const [isLoading, setLoading] = useState(false)
+  const [error, setError] = useState()
 
   const addIngredientHandler = ingredient => {
+    setLoading(true)
     fetch('https://react-hooks-ingredients-7320e.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
       headers: {'Content-Type': 'application/json'}
     }).then(response => {
+      setLoading(false)
       return response.json()
     }).then(responseData => {
       setUserIngredients(previousState=> (
         [...previousState, {...ingredient, id: responseData.name}]
       ))
+    }).catch(error=> {
+      setError(error.message)
     })
   }
   const removeIngredientHandler = ingredientId => {
+    setLoading(true)
     fetch(`https://react-hooks-ingredients-7320e.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE'
     }).then(() => {
+      setLoading(false);
       setUserIngredients(previousState=> (
         previousState.filter(ingredient => ingredient.id !== ingredientId)
       ))
+    }).catch(error => {
+      setError(error.message)
     })
   }
 
@@ -34,9 +45,15 @@ const Ingredients = () => {
     setUserIngredients(ingredients)
   },[])
 
+  const clearError = () => {
+    setError(null)
+    setLoading(false)
+  }
+
   return (
     <div className="App">
-      <IngredientForm addIngredient={addIngredientHandler}/>
+      {error && <ErrorModal onClose={clearError}>error</ErrorModal>}
+      <IngredientForm addIngredient={addIngredientHandler} loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
